47437ed3ea0ce8ac2095bae4f02019927f01a74b
hsnfqh <ahmadhasanfaqih01@gmail.com>
2026-08-31 15:11:54 +0700

    fix: save attendance base64 photo directly using native file_put_contents to eliminate finfo extension dependency

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -312,8 +312,16 @@ class AttendanceController extends Controller
         $imageData = preg_replace('/^data:image\/\w+;base64,/', '', $base64);
         $imageData = base64_decode($imageData);
 
-        $filename  = "attendance/{$userId}_{$type}_" . now()->format('Ymd_His') . '.jpg';
-        Storage::disk('public')->put($filename, $imageData);
+        $filename = "attendance/{$userId}_{$type}_" . now()->format('Ymd_His') . '.jpg';
+        $fullPath = storage_path('app/public/' . $filename);
+        $dir      = dirname($fullPath);
+
+        if (!file_exists($dir)) {
+            @mkdir($dir, 0755, true);
+        }
+
+        // Tulis file langsung tanpa bergantung pada ekstensi PHP fileinfo / finfo
+        file_put_contents($fullPath, $imageData);
 
         return $filename;
     }
