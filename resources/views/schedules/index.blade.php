0f4148c693a2b42b0bc2a3b0a446cf3099253494
hsnfqh <ahmadhasanfaqih01@gmail.com>
2026-09-08 09:42:37 +0700

    fix: restore Day Off engineers in Jadwal Kerja availability panel so all 9 team engineers are displayed

--- resources/views/schedules/index.blade.php
+++ resources/views/schedules/index.blade.php
@@ -1659,10 +1659,7 @@
                     });
                 },
                 get filteredEngineerAvailability() {
-                    var list = this.engineerAvailability.filter(function(e) {
-                        return !e.isDayOff;
-                    });
-                    return this.showOnlyAvailable ? list.filter(function(e) { return e.available; }) : list;
+                    return this.showOnlyAvailable ? this.engineerAvailability.filter(function(e) { return e.available; }) : this.engineerAvailability;
                 },
                 isEngineerBusyOnDate: function(engineerId, date) {
                     if (!date) return false;
