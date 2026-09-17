9ffc708d9fb582ddd4d19fc9258253dc6833b1e4
hsnfqh <ahmadhasanfaqih01@gmail.com>
2026-09-17 10:25:38 +0700

    chore(seeders): include DummyUserSeeder in default DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleSeeder::class,
+            DummyUserSeeder::class,
             DummyDataSeeder::class,
             SalesInitialSeeder::class,
             NotificationSeeder::class,
